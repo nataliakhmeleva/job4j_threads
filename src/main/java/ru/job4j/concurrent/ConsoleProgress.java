093c7218f0bfe1d8c20e98065b94bcf320c0f2a3
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-11-29 12:58:12 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            String[] process = {"-", "\\", "|", "/"};
+            if (count == process.length) {
+                count = 0;
+            }
+            System.out.print("\r load: " + process[count]);
+            count++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+}
