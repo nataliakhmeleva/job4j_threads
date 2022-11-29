358612f39aa6f2fdfd5acae6655f2e28b702d465
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-11-29 10:53:54 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                }
+        );
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " + second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
+            System.out.println(second.getName() + " " + second.getState());
+        }
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " + second.getState());
+        System.out.println("работа завершена");
+    }
+}
