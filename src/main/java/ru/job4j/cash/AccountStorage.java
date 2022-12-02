964da6be8ba459fc9d34b3584e1bee779e62e6e3
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-12-02 12:18:41 +0300

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,42 @@
+package ru.job4j.cash;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class AccountStorage {
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    public synchronized boolean delete(int id) {
+        return accounts.remove(id) != null;
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        if (accounts.containsKey(id)) {
+            return Optional.of(accounts.get(id));
+        }
+        return Optional.empty();
+
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        boolean rsl = false;
+        Optional<Account> srcAccount = getById(fromId);
+        Optional<Account> destAccount = getById(toId);
+        if (srcAccount.isPresent() && destAccount.isPresent()
+                && srcAccount.get().amount() >= amount) {
+            update(new Account(fromId, srcAccount.get().amount() - amount));
+            update(new Account(toId, destAccount.get().amount() + amount));
+            rsl = true;
+        }
+        return rsl;
+    }
+}
+
