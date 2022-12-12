1a612ea63e8e7bf81390894b546a8837d0ceceb7
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-12-12 12:23:10 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,37 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void send(String subject, String body, String email) {
+    }
+
+    private void emailTo(User user) {
+        String subject = "Notification %sto email %s.".formatted(user.getUsername(), user.getEmail());
+        String body = "Add a new event to %s.".formatted(user.getUsername());
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                send(subject, body, user.getEmail());
+            }
+        });
+        close();
+    }
+
+    private void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Execute " + Thread.currentThread().getName());
+    }
+}
