16b1d7d805c6d6a144f62f48c756ea65d5cce018
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-12-14 09:56:23 +0300

    4. CompletableFuture [#361626 #370700]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -4,41 +4,6 @@ import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public Sums(int rowSum, int colSum) {
-            this.rowSum = rowSum;
-            this.colSum = colSum;
-        }
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Sums sums = (Sums) o;
-            return rowSum == sums.rowSum && colSum == sums.colSum;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(rowSum, colSum);
-        }
-    }
-
     public static Sums[] sum(int[][] matrix) {
         Sums[] sums = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
